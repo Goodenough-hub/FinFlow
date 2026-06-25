936a08d4b41a2f3e5c6696d9dab4ddbd6adb7574
fangming <fangming@infini-ai.com>
2026-06-26 00:57:56 +0800

    Initial Commit

---
+++ FinFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinFlow
+//
+//  Created by fangming on 2026/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinFlow/FinFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinFlowApp.swift
+//  FinFlow
+//
+//  Created by fangming on 2026/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FinFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
